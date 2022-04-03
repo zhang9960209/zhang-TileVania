536aad6b03a4a2857e5202a738297c0d4e213926
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-04-03 18:51:49 -0400

    Horizontal Move

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -5,16 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerMov : MonoBehaviour
 {
+    [SerializeField] float movSpeed = 10f;
     Vector2 movInput;
+    Rigidbody2D playerRigid;
    
     void Start()
     {
-        
+        playerRigid = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
+        Mov();
+        
         
     }
 
@@ -24,4 +28,12 @@ public class PlayerMov : MonoBehaviour
         movInput = value.Get<Vector2>();
         Debug.Log(movInput);
     }
+
+    void Mov()
+    { 
+        Vector2 playerVel = new Vector2(movInput.x * movSpeed, playerRigid.velocity.y);
+        playerRigid.velocity = playerVel; 
+    
+    }
+  
 }
