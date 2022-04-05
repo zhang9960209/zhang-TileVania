95dbe24834fe03e3e7eb823a28b32a97cc69965f
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-04-05 15:15:08 -0400

    Animation State In Code

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -8,18 +8,19 @@ public class PlayerMov : MonoBehaviour
     [SerializeField] float movSpeed = 10f;
     Vector2 movInput;
     Rigidbody2D playerRigid;
+    Animator playerAnimator;
    
     void Start()
     {
         playerRigid = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
 
     void Update()
     {
         Mov();
-        
-        
+        FlipPlayer();
     }
 
     
@@ -32,8 +33,19 @@ public class PlayerMov : MonoBehaviour
     void Mov()
     { 
         Vector2 playerVel = new Vector2(movInput.x * movSpeed, playerRigid.velocity.y);
-        playerRigid.velocity = playerVel; 
-    
+        playerRigid.velocity = playerVel;
+        bool blHasHorizontalSpeed = Mathf.Abs(playerRigid.velocity.x) > Mathf.Epsilon;
+        playerAnimator.SetBool("isRunning", blHasHorizontalSpeed);
+    }
+
+    void FlipPlayer()
+    {
+        bool blHasHorizontalSpeed = Mathf.Abs(playerRigid.velocity.x) > Mathf.Epsilon;
+        if (blHasHorizontalSpeed) 
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerRigid.velocity.x), 1f);
+        }
+        
     }
   
 }
